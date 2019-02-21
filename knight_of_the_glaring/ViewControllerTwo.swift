539ef2e10aafb6c514849488e0e1c88d713cea36
Jordan Morgan <jordan.morgan2@smail.astate.edu>
2019-02-21 15:09:54 -0600

    initial commit of project

---
+++ knight_of_the_glaring/ViewControllerTwo.swift
@@ -0,0 +1,19 @@
+//
+//  ViewControllerTwo.swift
+//  knight_of_the_glaring
+//
+//  Created by Jordan.Morgan2 on 2/21/19.
+//  Copyright © 2019 Jordan.Morgan2. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
